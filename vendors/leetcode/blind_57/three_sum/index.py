3600770b06b32582c63df29e8095900d0eac3b3d
msolorio <msolorio.github@gmail.com>
2022-05-24 22:11:31 -0700

    [wip] writing solution for three_sum

---
+++ vendors/leetcode/blind_57/three_sum/index.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    # triple nested loop
+    # O(n^3)
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        pass
+        # sort numbers into positives, negatives, and zeros
+
+
+        # If zero in the list, see if negative and positive opposites exist (-1, 1)
+
+
+
+        # check if 3 zeros exist
+
+
+
+        # for all negative pairs see if positive complement exists (-1, -1, 2)
+
+
+        # for all positive pairs see if negative complement exists
+        
